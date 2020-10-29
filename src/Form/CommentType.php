344bf9ea32e7636f5babf91f1ea46eed3eaf9af4
Paul Cottin <cottin.paul45@gmail.com>
2020-10-29 12:47:35 +0100

    Comment systeme : form, display comments, paging loadMore

---
+++ src/Form/CommentType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+Class CommentType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('comment', TextType::class, [
+                'label' => 'Votre commentaire : ',
+            ])
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
